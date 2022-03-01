fbc0622cfe61fb1005b734443925713b8af16992
Jehovanie <jehovanieram@gmail.com>
2022-03-01 22:50:03 +0300

    Mettre disign  pages dashboard , form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeaturesComponent } from './pages/features/features.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MembreComponent } from './pages/membre/membre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "membre", component: MembreComponent },
+  { path: "form", component: FeaturesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './elements/menu/menu.component';
+import { FooterComponent } from './elements/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MembreComponent } from './pages/membre/membre.component';
+import { FeaturesComponent } from './pages/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    MembreComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,

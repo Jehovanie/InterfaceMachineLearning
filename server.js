4c3afc161c2f8f94d90b7b0aed356c5ec50ed5d6
Jehovanie <jehovanieram@gmail.com>
2022-03-02 15:01:54 +0300

    resolve not found in heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/price-of-house'));
+app.use(express.static(__dirname + '/dist/priceOfHouse'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/price-of-house/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/priceOfHouse/index.html'));
 });
 app.listen(process.env.PORT || 8080);
 

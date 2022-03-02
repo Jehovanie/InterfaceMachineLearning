69b9920fc9e31b807f2e97014710fc8ef09c837f
Jehovanie <jehovanieram@gmail.com>
2022-03-02 14:44:31 +0300

    reset updates to deploy to heroku

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/price-of-house'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/<app-name>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/price-of-house/index.html'));
 });
 app.listen(process.env.PORT || 8080);
+
